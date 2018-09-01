d456e5f4daf18c5fe87478df0e85d88309613db8
Shaun Church <shaun@shaunchurch.com>
2018-09-01 19:46:10 +0100

    Simplify defaults

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,51 +2,30 @@ import styled, { injectGlobal, css } from 'styled-components';
 import styledNormalize from 'styled-normalize';
 
 /**
- * Global base typography
- */
-const configureType = css`
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
-    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  font-size: 62.5%; /* 1em = 10px */
-`;
-
-const bodyType = css`
-  font-size: 1.8em;
-  line-height: 1.6em;
-`;
-
-/**
  * Minimal global styling. Use sparingly or not at all 😇
  */
 injectGlobal`
   ${styledNormalize}
 
-  /**
-  * Universal border-box box-sizing.
-  */
   html {
     box-sizing: border-box;
+    font-size: 62.5%; /* 1em = 10px */
   }
+
   *,
   *::before,
   *::after {
     box-sizing: inherit;
   }
 
-  html {
-    ${configureType}
-  }
-
   body {
-    ${bodyType}
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    font-size: 1.8em;
+    line-height: 1.6em;
+    color: #222;
   }
-
-`;
-
-export const defaultMargin = css`
-  margin: ${props => props.theme.margin}em;
 `;
 
 export const Page = styled.section`
-  ${defaultMargin};
+  margin: ${props => props.theme.margin}em;
 `;
--- src/defaultTheme.js
+++ src/defaultTheme.js
@@ -1,3 +1,4 @@
 export default {
   margin: 1.4,
+  foreground: '#222',
 };
